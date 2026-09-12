60ae05f811287b34ab02c048cd881f487c12f9cd
evaguenegou-art <eva.guenegou@iut-rodez.fr>
2026-09-12 15:39:16 +0200

    ajout image et gradient

---
+++ TP1-LectureEcritureJava/Gradient.java
@@ -0,0 +1,22 @@
+public class Gradient {
+    public static void main(String[] args) {
+        Image img = new Image(200, 100);
+		int hauteur = img.getHeight();
+		int largeur = img.getWidth();
+
+        // Génération du dégradé de bleu
+        for (int y = 0; y < hauteur; y++) {
+            for (int x = 0; x < largeur; x++) {
+                int bleu = (x * 255) / (200 - 1);
+                img.setPixel(x, y, 0, 0, bleu);
+            }
+        }
+
+        try {
+            img.save_txt("gradient.ppm");
+            System.out.println("Dégradé créé avec succès !");
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
+        }
+    }
+}
