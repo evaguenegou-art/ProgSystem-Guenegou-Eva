cfa352f8683daf4e06cfb60d5af3e430f9a94d94
EVA GUENEGOU <eva.guenegou@iut.local>
2026-09-16 10:36:09 +0200

    début de Utils.java + ajout des tests

---
+++ TP2-SystemeFichiersVirtuel/Utils.java
@@ -0,0 +1,28 @@
+public class Utils {
+
+    public static int writeInt(byte[] memory, int offset, int value) {
+        // TODO: Écrire les 4 octets de 'value' dans 'memory'
+        // à partir de 'offset', en big-endian.
+		byte b;
+		for (int decalage = 0; decalage < 4; decalage++){
+			b = (byte)(value >> decalage*8 & 0xFF);
+			memory[offset + decalage] = b;
+		}
+		return 4;
+    }
+
+    public static int readInt(byte[] memory, int offset) {
+        // TODO: Reconstituer le int sur 4 octets.
+        return 0;
+    }
+
+    public static int writeShort(byte[] memory, int offset, short value) {
+        // TODO: Écrire les 2 octets de 'value'.
+        return 2;
+    }
+
+    public static short readShort(byte[] memory, int offset) {
+        // TODO: Lire le short sur 2 octets.
+        return 0;
+    }
+}
