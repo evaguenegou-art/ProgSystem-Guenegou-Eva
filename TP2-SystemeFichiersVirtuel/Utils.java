cebb66b2d9d7643de41045bf88f4767886945337
evaguenegou-art <eva.guenegou@iut-rodez.fr>
2026-09-23 20:52:57 +0200

    etape 2 - TP2 mercredi soir

--- TP2-SystemeFichiersVirtuel/Utils.java
+++ TP2-SystemeFichiersVirtuel/Utils.java
@@ -5,7 +5,7 @@ public class Utils {
         // à partir de 'offset', en big-endian.
 		byte b;
 		for (int decalage = 0; decalage < 4; decalage++){
-			b = (byte)(value >> decalage*8 & 0xFF);
+			b = (byte)(value >> (3-decalage)*8 & 0xFF);
 			memory[offset + decalage] = b;
 		}
 		return 4;
@@ -13,16 +13,31 @@ public class Utils {
 
     public static int readInt(byte[] memory, int offset) {
         // TODO: Reconstituer le int sur 4 octets.
-        return 0;
+		int i = 0;
+		for (int decalage = 0; decalage < 4; decalage++){
+			int value = memory[offset + decalage] & 0xFF;
+			i = i | (value << (3-decalage)*8);
+		}
+        return i;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
         // TODO: Écrire les 2 octets de 'value'.
+		byte b;
+		for (int decalage = 0; decalage < 2; decalage++){
+			b = (byte)(value >> (1-decalage)*8 & 0xFF);
+			memory[offset + decalage] = b;
+		}
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
         // TODO: Lire le short sur 2 octets.
-        return 0;
+		int i = 0;
+		for (int decalage = 0; decalage < 2; decalage++){
+			int value = memory[offset + decalage] & 0xFF;
+			i = i | (value << (1-decalage)*8);
+		}
+        return (short)i;
     }
 }
