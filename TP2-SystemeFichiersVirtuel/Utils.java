9aa53aab3adfc4f1a5e03c107cbd5f12fe0d349f
evaguenegou-art <eva.guenegou@iut-rodez.fr>
2026-09-24 18:22:36 +0200

    réalisation étape 3

--- TP2-SystemeFichiersVirtuel/Utils.java
+++ TP2-SystemeFichiersVirtuel/Utils.java
@@ -1,7 +1,7 @@
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
-        // TODO: Écrire les 4 octets de 'value' dans 'memory'
+        // Écrire les 4 octets de 'value' dans 'memory'
         // à partir de 'offset', en big-endian.
 		byte b;
 		for (int decalage = 0; decalage < 4; decalage++){
@@ -12,7 +12,7 @@ public class Utils {
     }
 
     public static int readInt(byte[] memory, int offset) {
-        // TODO: Reconstituer le int sur 4 octets.
+        // Reconstituer le int sur 4 octets.
 		int i = 0;
 		for (int decalage = 0; decalage < 4; decalage++){
 			int value = memory[offset + decalage] & 0xFF;
@@ -22,7 +22,7 @@ public class Utils {
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
+        // Écrire les 2 octets de 'value'.
 		byte b;
 		for (int decalage = 0; decalage < 2; decalage++){
 			b = (byte)(value >> (1-decalage)*8 & 0xFF);
@@ -32,7 +32,7 @@ public class Utils {
     }
 
     public static short readShort(byte[] memory, int offset) {
-        // TODO: Lire le short sur 2 octets.
+        // Lire le short sur 2 octets.
 		int i = 0;
 		for (int decalage = 0; decalage < 2; decalage++){
 			int value = memory[offset + decalage] & 0xFF;
@@ -40,4 +40,53 @@ public class Utils {
 		}
         return (short)i;
     }
+	
+	public static int writeLong(byte[] memory, int offset, long value) {
+		// Écrire les 8 octets du long en big-endian.
+		byte b;
+		for (int decalage = 0; decalage < 8; decalage++){
+			b = (byte)(value >> (7-decalage)*8 & 0xFF);
+			memory[offset + decalage] = b;
+		}
+		return 8;
+	}
+
+	public static long readLong(byte[] memory, int offset) {
+		// Reconstituer le long.
+		long i = 0L;
+		for (int decalage = 0; decalage < 8; decalage++){
+			long value = memory[offset + decalage] & 0xFF;
+			i = i | (value << (7-decalage)*8);
+		}
+		return i;
+	}
+
+	public static int writeString(byte[] memory,int offset,
+									String str,int maxLength) {
+		// 1. Convertir la chaîne en octets.
+		byte[] bytes = str.getBytes();
+		// 2. Copier les octets sans dépasser maxLength.
+		// 3. Nettoyer le reste de la zone avec des zéros.
+		for (int decalage = 0; decalage < maxLength; decalage++) {
+			if (decalage < bytes.length) {
+				memory[offset + decalage] = bytes[decalage]; // Étape 2 : copie
+			} else {
+				memory[offset + decalage] = 0;               // Étape 3 : nettoyage
+			}
+		}
+		return maxLength;
+	}
+
+	public static String readString(byte[] memory,int offset,
+										int maxLength) {
+		int decalage = 0;
+		// Lire jusqu'au premier octet nul
+		// ou jusqu'à maxLength.
+		while (decalage < maxLength && memory[offset + decalage] != 0) {
+			decalage++;
+		}
+		return new String(memory, offset, decalage);
+	}
+
 }
+
